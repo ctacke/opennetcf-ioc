c4ef018be676e4b218fa65d5f8805a6a6072c305
Chris Tacke <Chris Tacke>
2016-07-21 11:56:24 -0500

    Split classes into their own files

--- OpenNETCF.IoC/Properties/AssemblyInfo.cs
+++ OpenNETCF.IoC/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OpenNETCF.IoC.Mini")]
+[assembly: AssemblyTitle("OpenNETCF.IoC.PCL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tacke Consulting, LLC")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.16201.0")]
-[assembly: AssemblyFileVersion("1.0.16201.0")]
+[assembly: AssemblyVersion("1.0.16202.0")]
+[assembly: AssemblyFileVersion("1.0.16202.0")]
