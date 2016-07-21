2bc09d64244caa521a747f2f91e69cb82561800e
Chris Tacke <Chris Tacke>
2016-07-21 15:09:45 -0500

    Initial NavigationSample check-in

---
+++ Samples/NavigationSample/NavigationSample/ViewModels/HomeViewModel.cs
@@ -0,0 +1,29 @@
+﻿using OpenNETCF.IoC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+using NavigationSample.Views;
+
+namespace NavigationSample.ViewModels
+{
+    class HomeViewModel : IViewModel
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand ShowDetailsCommand
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    NavigationService.NavigateForward<DetailsView>(true);
+                });
+            }
+        }
+    }
+}
---
+++ Samples/NavigationSample/NavigationSample/Views/DetailsView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace NavigationSample.Views
+{
+    public partial class DetailsView : ContentPage
+    {
+        public DetailsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
