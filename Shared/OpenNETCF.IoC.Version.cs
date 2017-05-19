2e3de3e4fe44ae07cd6ad1aaaff9bdd45c0dd914
ctacke <ctacke@opennetcf.com>
2017-05-19 17:25:56 -0500

    Removed methods duplicated from Extensions

--- Shared/OpenNETCF.IoC.Version.cs
+++ Shared/OpenNETCF.IoC.Version.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.17135.0")]
-[assembly: AssemblyFileVersion("1.0.17135.0")]
+[assembly: AssemblyVersion("1.0.17139.0")]
+[assembly: AssemblyFileVersion("1.0.17139.0")]
 [assembly: AssemblyCompany("Tacke Consulting, LLC")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 
