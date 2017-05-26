d9a4ca097c5f20a41d84c27561202232d7e6f188
ctacke <ctacke@opennetcf.com>
2017-05-26 15:24:18 -0500

    Fixing broken nuget package

--- Shared/OpenNETCF.IoC.Version.cs
+++ Shared/OpenNETCF.IoC.Version.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.17146.0")]
-[assembly: AssemblyFileVersion("1.0.17146.0")]
+[assembly: AssemblyVersion("1.0.17146.1")]
+[assembly: AssemblyFileVersion("1.0.17146.1")]
 [assembly: AssemblyCompany("Tacke Consulting, LLC")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 
